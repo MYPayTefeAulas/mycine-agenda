68365c8ec56007dc805b596fb34f80a2c6e0edaa
Robert <rribeiro@uea.edu.br>
2021-10-21 14:22:51 -0400

    Agenda V3 - Ok

--- Agenda/app/src/main/java/com/uea/mypay/agenda/AjustesFragment.kt
+++ Agenda/app/src/main/java/com/uea/mypay/agenda/AjustesFragment.kt
@@ -18,6 +18,7 @@ class AjustesFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentAjustesBinding.inflate(inflater,container,false)
+
         return  binding.root
     }
 }
--- Agenda/app/src/main/java/com/uea/mypay/agenda/TelaInicialActivity.kt
+++ Agenda/app/src/main/java/com/uea/mypay/agenda/TelaInicialActivity.kt
@@ -6,14 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.uea.mypay.agenda.databinding.ActivityTelaInicialBinding
 
 class TelaInicialActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTelaInicialBinding
-
-//    private lateinit var adapter: ArrayAdapter<Contato>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,20 +24,14 @@ class TelaInicialActivity : AppCompatActivity() {
             .replace(R.id.fragmentContainer, ListaContatosFragment())
             .commit()
 
-        binding.bottomNavigationView4.setOnNavigationItemReselectedListener {
+        binding.bottomNavigationView4.setOnNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.ic_home -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragmentContainer, ListaContatosFragment())
-                        .commit()
+                    loadFragments(ListaContatosFragment(), FRAGMENT_HOME)
                     true
                 }
                 R.id.ic_ajustes -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragmentContainer, AjustesFragment())
-                        .commit()
+                    loadFragments(AjustesFragment(), FRAGMENT_AJUSTES)
                     true
                 }
                 else ->
@@ -49,6 +42,17 @@ class TelaInicialActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
+    //Carrega os fragments e os empilha
+    private fun loadFragments(fragment: Fragment, tag: String){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, fragment, tag)
+            commit()
+        }
+    }
 
+    companion object {
+        private const val FRAGMENT_HOME = "FRAGMENT_HOME"
+        private const val FRAGMENT_AJUSTES = "FRAGMENT_AJUSTES"
+    }
 
 }
